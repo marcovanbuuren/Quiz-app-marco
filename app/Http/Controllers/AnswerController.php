166b3bd3461fccddbd4e8802435eac61864e8b09
marcovanbuuren <117274420+marcovanbuuren@users.noreply.github.com>
2024-09-08 19:20:39 +0200

    AnswerController bijgevult + regel verwijderd bij model Question
    de methodes in de AnswerController hebben nu code gekregen voor de functie die de methodes geplant waren om uit te voeren. Er is een regel code verwijderd bij de model Question over de relatie met andere models

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,23 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Models\UserQuestion;
 use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
     //
-    public function getAnswer()
+    public function getAnswer($question_id)
     {
-        
+        $question = Question::where('id', $id);
+        return $question->correct_answer;
     }
 
-    public function controlAnswer()
+    public function controlAnswer($user_answer, $correct_answer)
     {
-
+        if ($user_answer == $correct_answer) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 
-    public function storeAnswer()
+    public function storeAnswer(Request $request)
     {
-        
+        $request->validate([
+            'user_answer' => 'required|string|max:255',
+            'question_id' => 'required|string|max:255',
+        ]);
+
+        $correct_answer = getAnswer($request->question_id);
+        $evaluated_answer = controlAnswer($request->user_answer, $correct_answer);
+
+        $answer = new UserQuestion();
+        $answer->question_id = $request->question_id;
+        $answer->user_id = Auth::user()->id;
+        $answer->user_answer = $request->user_answer;
+        $answer->save();
+
+        return redirect()->route('question.get.student');
     }
 }
